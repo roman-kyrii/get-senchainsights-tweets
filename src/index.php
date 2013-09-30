ee29e6d82e5bd72176e878650a27ea3ed0727731
Roman Kyrii <roman.kyrii@mev.com>
2013-10-01 00:34:17 +0300

    redone

--- src/index.php
+++ src/index.php
@@ -3,15 +3,12 @@
 require '../vendor/autoload.php';
 
 use Guzzle\Http\Client;
+use Guzzle\Plugin\Oauth\OauthPlugin;
 
 $baseUrl = 'https://api.twitter.com/1.1/';
-
 $resource = 'statuses/user_timeline.json';
-
 $screenName = 'SenchaInsights';
 $count = 5;
-
-$url = $baseUrl . $resource . '?screen_name=' . $screenName . '&count=' . $count;
 
 $consumerKey = 'euoxQYDK14UvIIfACsbC9g';
 $consumerSecret = 'j7VUsKQUvGzCXqmNnrVmN6W9up9D5XfRSr09z6WQp9k';
@@ -20,5 +17,22 @@ $accessTokenSecret = '1zB5nJOGnbjuLjYVwMF77NUdQyY75ICrBPCYhXmx9E';
 
 $client = new Client($baseUrl);
 
+$setting = array(
+    'consumer_key'    => $consumerKey,
+    'consumer_secret' => $consumerSecret,
+    'token'           => $accessToken,
+    'token_secret'    => $accessTokenSecret
+);
 
+$client->addSubscriber(new OauthPlugin($settings));
+
+$request = $client->get($resource);
+$request->getQuery()->set('screen_name', $screenName);
+$request->getQuery()->set('count', $count);
+
+$response = $request->send();
+
+$tweets = json_decode($resonse->getBody());
+
+?>
 
